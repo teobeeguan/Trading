749f828c50a10df36e8d61538eedf13dfca5aaf4
osamkz <93231011+osamkz@users.noreply.github.com>
2022-04-22 03:53:19 +0200

    Update StockScreenDemo.py

--- StockScreenDemo.py
+++ StockScreenDemo.py
@@ -18,7 +18,11 @@ from plotly.subplots import make_subplots
 snp500 = pd.read_csv("Datasets/SP500.csv")
 symbols = snp500['Symbol'].sort_values().tolist()        
 
-
+st.write("""
+#RIVACUBE ELEMENTARY SCREENER 
+First Jet with Fundamental, Technical and Intraday analysis
+*Built by SLM for Rivaldi Project*
+""")
 ticker = st.sidebar.selectbox(
     'Choose a S&P 500 Stock',
      symbols)
