b9eb45a511b72ef2757e7427db1bc6869b679eea
osamkz <93231011+osamkz@users.noreply.github.com>
2022-04-22 04:12:58 +0200

    Update StockScreenDemo.py

--- StockScreenDemo.py
+++ StockScreenDemo.py
@@ -70,7 +70,6 @@ if(infoType == 'Fundamental'):
             'Dividend Yield (%)': info['dividendYield'],
             'Five year Avg Dividend Yield (%)': info['fiveYearAvgDividendYield'],
             'Payout Ratio': info['payoutRatio'],
-            'ISIN Number': info['isin'],
         }
     
     fundDF = pd.DataFrame.from_dict(fundInfo, orient='index')
