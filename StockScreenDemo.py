1b81edbd089e3e323c9d5e0a6bc3ecbd445b39db
osamkz <93231011+osamkz@users.noreply.github.com>
2022-05-10 22:33:29 +0200

    Update StockScreenDemo.py

--- StockScreenDemo.py
+++ StockScreenDemo.py
@@ -317,10 +317,29 @@ if(infoType == 'Technical'):
     figBoll.update_yaxes(tickprefix="$")
     st.plotly_chart(figBoll, use_container_width=True)
 if(infoType == 'Intraday'):
+    
+    st.subheader('Intraday per dates')
+        d1 = st.date_input(
+     "Choose the beginning date",
+     datetime.date(2022, 4, 5))
+    st.write('From:', d1)
+    d2 = st.date_input(
+     "Choose the end date",
+     datetime.date(2022, 4, 7))
+    st.write('To:', d2)
+
+    stock = yf.Ticker(ticker)
 
     
-    st.subheader('Intraday per interval')
-    inter=st.selectbox("Choose the interval",["1m","2m","5m","15m","30m","60m","90m","1h"])
+    Intraday = pdr.DataReader(ticker, data_source="yahoo",start=d1,end=d2)
+    st.table(Intraday)
+    def convert_df(df):
+        return df.to_csv().encode('utf-8')
+
+
+    
+    st.subheader('Intraday with interval')
+    inter=st.selectbox("Choose the interval",["1m","2m","5m","15m","30m","60m","90m"])
     d1 = st.date_input(
      "Choose the beginning date",
      datetime.date(2022, 4, 5))
