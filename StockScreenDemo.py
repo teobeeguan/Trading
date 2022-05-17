0eaef41b2529f86941953e1e6153124cd880730c
osamkz <93231011+osamkz@users.noreply.github.com>
2022-05-17 21:19:51 +0200

    Update StockScreenDemo.py

--- StockScreenDemo.py
+++ StockScreenDemo.py
@@ -12,8 +12,10 @@ import investpy
 import yfinance as yf
 import streamlit as st
 import datetime as dt
+import numpy as np
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+from keras.models import load_model
 
 st.sidebar.image("https://slm-sa.com/wp-content/uploads/2020/01/LogoSLM-IF-1.png")
 st.sidebar.write("""
@@ -37,7 +39,7 @@ traday = pdr.DataReader(ticker, data_source="yahoo")
 
 infoType = st.sidebar.radio(
         "Choose an info type",
-        ('Fundamental', 'Technical', 'Intraday')
+        ('Fundamental', 'Technical', 'Intraday', 'Prediction')
     )
 
 stock = yf.Ticker(ticker)
@@ -363,3 +365,87 @@ if(infoType == 'Intraday'):
    "text/csv",
    key='download-csv'
     )
+if(infoType == 'Prediction'):
+    start = '1999-01-01'
+    end = '2001-12-31'
+
+    st.title("stock prediction")
+    user_input=ticker
+    df=data.DataReader(user_input, 'yahoo', start, end)
+
+    st.subheader("data from 2010-2021")
+    st.write(df.describe())
+
+    st.subheader("CLOSING PRICE VS TIME CHART")
+    fig=plt.figure(figsize=(12,6))
+    plt.plot(df.Close)
+    st.pyplot(fig)
+
+    st.subheader("CLOSING PRICE VS TIME CHART & 100MA")
+    ma100=df.Close.rolling(100).mean()
+    fig=plt.figure(figsize=(12,6))
+    plt.plot(ma100, label='Mean 100')
+    plt.plot(df.Close)
+    plt.legend()
+    st.pyplot(fig)
+
+    st.subheader("CLOSING PRICE VS TIME CHART & 100MA and 200MA")
+    ma100=df.Close.rolling(100).mean()
+    ma200=df.Close.rolling(200).mean()
+    fig=plt.figure(figsize=(12,6))
+    plt.plot(ma100, label='Mean 100')
+    plt.plot(ma200,label='Mean 200')
+    plt.plot(df.Close)
+    plt.legend()
+    st.pyplot(fig)
+
+
+    #splittng data into data frame
+    data_train=pd.DataFrame(df['Close'][0:int(len(df)*0.70)])
+    data_test=pd.DataFrame(df['Close'][int(len(df)*0.70):int(len(df))])
+
+    from sklearn.preprocessing import MinMaxScaler
+    scaler=MinMaxScaler(feature_range=(0,1))
+
+    data_train_array=scaler.fit_transform(data_train)
+
+    x_train=[]
+    y_train=[]
+
+    #splitting data into xtrain and ytrain
+    for i in range(100, data_train_array.shape[0]):
+        x_train.append(data_train_array[i-100:i])
+        y_train.append(data_train_array[i,0])
+    x_train=np.array(x_train)
+    y_train=np.array(y_train)
+
+    #load model
+    model=load_model('keras_model.h5')
+    past_100_days=data_train.tail(100)
+    final_df=past_100_days.append(data_test, ignore_index=True)
+    input_data=scaler.fit_transform(final_df)
+
+    #testing
+    x_test=[]
+    y_test=[]
+
+    for i in range(100, input_data.shape[0]):
+        x_test.append(input_data[i-100:i])
+        y_test.append(input_data[i,0])
+    x_test,y_test= np.array(x_test),np.array(y_test)
+    y_predicted=model.predict(x_test)
+
+    scaler=scaler.scale_
+    scaler_factor=1/scaler[0]
+    y_predicted=y_predicted*scaler_factor
+    y_test=y_test*scaler_factor
+
+    # showing
+    st.subheader("PREDICTION VS TIME ORIGINAL PRICE")
+    fig2=plt.figure(figsize=(12,6))
+    plt.plot(y_test,'b', label='Original Price')
+    plt.plot(y_predicted,'r',label='Predicted Price')
+    plt.xlabel('Time')
+    plt.ylabel('Price')
+    plt.legend()
+    st.pyplot(fig2)
