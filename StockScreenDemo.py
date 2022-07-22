137a231ef4134d5d799eb25d066b502dca8ef78b
osamkz <93231011+osamkz@users.noreply.github.com>
2022-07-22 04:34:40 +0200

    Update StockScreenDemo.py

--- StockScreenDemo.py
+++ StockScreenDemo.py
@@ -631,10 +631,10 @@ if(infoType == 'ESG'):
    st.plotly_chart(fig)
 
 if(infoType == 'Media'):
-	def show_pdf(file_path):
-    		with open(file_path,"rb") as f:
-        	base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    		pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
-    		st.markdown(pdf_display, unsafe_allow_html=True)
+   def show_pdf(file_path):
+	with open(file_path,"rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+    	pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
+    	st.markdown(pdf_display, unsafe_allow_html=True)
 
-	show_pdf('Media/mediaDash Twitter ADOBE.pdf')
+   show_pdf('Media/mediaDash Twitter ADOBE.pdf')
