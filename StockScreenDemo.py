0466dbdc3afe498218f482c6b4bb082172cee2bd
osamkz <93231011+osamkz@users.noreply.github.com>
2022-07-22 04:13:04 +0200

    Update StockScreenDemo.py

--- StockScreenDemo.py
+++ StockScreenDemo.py
@@ -11,6 +11,7 @@ from pandas_datareader import data as pdr
 import investpy
 import yfinance as yf
 import streamlit as st
+import streamlit.components.v1 as components
 import datetime as dt
 import numpy as np
 import matplotlib.pyplot as plt
@@ -54,7 +55,7 @@ traday = pdr.DataReader(ticker, data_source="yahoo")
 
 infoType = st.sidebar.radio(
         "Choose an analysis type",
-        ('Fundamental', 'Technical', 'Intraday', 'Prediction', 'Sentiment', 'ESG')
+        ('Fundamental', 'Technical', 'Intraday', 'Prediction', 'Sentiment', 'Media', 'ESG')
     )
 
 stock = yf.Ticker(ticker)
