4e6261aef6a34d369d791a2b1b0f4e38ddd62328
osamkz <93231011+osamkz@users.noreply.github.com>
2022-07-22 04:49:40 +0200

    Update StockScreenDemo.py

--- StockScreenDemo.py
+++ StockScreenDemo.py
@@ -635,7 +635,7 @@ if(infoType == 'Media'):
    def displayPDF(file):
 	# Opening file from file path
 	with open(file, "rb") as f:
-		base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+	    base64_pdf = base64.b64encode(f.read()).decode('utf-8')
 	# Embedding PDF in HTML
 	pdf_display = F'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
 	# Displaying File
