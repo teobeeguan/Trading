5f1c799c2afd0a76144572a16487ecb3a5f48f61
osamkz <93231011+osamkz@users.noreply.github.com>
2022-07-22 04:54:18 +0200

    Update StockScreenDemo.py

--- StockScreenDemo.py
+++ StockScreenDemo.py
@@ -633,12 +633,14 @@ if(infoType == 'ESG'):
 
 if(infoType == 'Media'):
    def displayPDF(file):
-        # Opening file from file path
-        with open(file, "rb") as f:
-	     base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-        # Embedding PDF in HTML
-        pdf_display = F'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
-        # Displaying File
-        st.markdown(pdf_display, unsafe_allow_html=True)
+    # Opening file from file path
+    with open(file, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+
+    # Embedding PDF in HTML
+    pdf_display = F'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+
+    # Displaying File
+    st.markdown(pdf_display, unsafe_allow_html=True)
    
    show_pdf('Media/mediaDash Twitter ADOBE.pdf')
