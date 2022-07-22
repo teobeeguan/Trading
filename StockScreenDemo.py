fd3d8a6cb0b0809fa7a27b226c89f551062f6a69
osamkz <93231011+osamkz@users.noreply.github.com>
2022-07-22 04:43:07 +0200

    Update StockScreenDemo.py

--- StockScreenDemo.py
+++ StockScreenDemo.py
@@ -635,7 +635,7 @@ if(infoType == 'Media'):
    def show_pdf(file_path):
         with open(file_path,"rb") as f:
                base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-	       pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
+	       pdf_display = f'<embed src=”data:application/pdf;base64,{base64_pdf}” width=”700″ height=”1000″ type=”application/pdf”>’
 		
 	st.markdown(pdf_display, unsafe_allow_html=True)
 
