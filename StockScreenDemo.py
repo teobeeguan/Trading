eb10faf60bc3789843bdcab55b96133389d6cd9e
osamkz <93231011+osamkz@users.noreply.github.com>
2022-07-22 04:44:19 +0200

    Update StockScreenDemo.py

--- StockScreenDemo.py
+++ StockScreenDemo.py
@@ -636,6 +636,6 @@ if(infoType == 'Media'):
         with open(file_path,"rb") as f:
                base64_pdf = base64.b64encode(f.read()).decode('utf-8')
 	       pdf_display = f'<embed src=”data:application/pdf;base64,{base64_pdf}” width=”700″ height=”1000″ type=”application/pdf”>’
-		st.markdown(pdf_display, unsafe_allow_html=True)
+	       st.markdown(pdf_display, unsafe_allow_html=True)
 
    show_pdf('Media/mediaDash Twitter ADOBE.pdf')
