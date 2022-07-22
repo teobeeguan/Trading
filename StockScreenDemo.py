0707a795b77db4021d151f980b7427ff03c2eeda
osamkz <93231011+osamkz@users.noreply.github.com>
2022-07-22 04:36:16 +0200

    Update StockScreenDemo.py

--- StockScreenDemo.py
+++ StockScreenDemo.py
@@ -633,7 +633,7 @@ if(infoType == 'ESG'):
 if(infoType == 'Media'):
    def show_pdf(file_path):
 	with open(file_path,"rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+		base64_pdf = base64.b64encode(f.read()).decode('utf-8')
     	pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
     	st.markdown(pdf_display, unsafe_allow_html=True)
 
