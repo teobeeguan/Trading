7e7c9f651c7f009a332a6b290eecffa5f440f1e5
osamkz <93231011+osamkz@users.noreply.github.com>
2022-08-18 17:18:32 +0000

    Update StockScreenDemo.py

--- StockScreenDemo.py
+++ StockScreenDemo.py
@@ -163,7 +163,7 @@ if(infoType == 'Technical'):
     st.title('Technical Indicators')
     st.subheader('Moving Average')
     
-    coMA1, coMA2 = st.beta_columns(2)
+    coMA1, coMA2 = st.columns(2)
     
     with coMA1:
         numYearMA = st.number_input('Insert period (Year): ', min_value=1, max_value=10, value=2, key=0)    
@@ -286,7 +286,7 @@ if(infoType == 'Technical'):
     st.plotly_chart(figMACD, use_container_width=True)
     
     st.subheader('Bollinger Band')
-    coBoll1, coBoll2 = st.beta_columns(2)
+    coBoll1, coBoll2 = st.columns(2)
     with coBoll1:
         numYearBoll = st.number_input('Insert period (Year): ', min_value=1, max_value=10, value=2, key=6) 
         
