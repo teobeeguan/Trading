f148e001ffc43d4de7c39b7694c45211cc2ae800
osamkz <93231011+osamkz@users.noreply.github.com>
2022-08-26 10:56:00 +0000

    Update StockScreenDemo.py

--- StockScreenDemo.py
+++ StockScreenDemo.py
@@ -30,6 +30,9 @@ import nltk
 nltk.downloader.download('vader_lexicon')
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import plotly.express as px
+from prophet import Prophet
+from plotly import graph_objs as go
+from prophet.plot import plot_plotly
 
 
 
@@ -56,7 +59,7 @@ traday = pdr.DataReader(ticker, data_source="yahoo")
 
 infoType = st.sidebar.radio(
         "Choose an analysis type",
-        ('Fundamental', 'Technical', 'Intraday', 'Prediction', 'Sentiment', 'Media', 'ESG')
+        ('Fundamental', 'Technical', 'Intraday', 'Prediction', 'Sentiment', 'Media', 'ESG', 'DataSeer')
     )
 
 stock = yf.Ticker(ticker)
@@ -650,3 +653,87 @@ if(infoType == 'Media'):
    #displayPDF("Media/Twitter_ADOBE.pdf")
    p = open("Media/Impact_period2.html", 'r')
    components.iframe(p.read())
+	
+	
+if(infoType == 'DataSeer'):
+   
+   tickerSymbol = ticker
+   tickerData = yf.Ticker(tickerSymbol)
+   alpha2,gamma2 = st.columns(2)
+   starting = alpha2.date_input('Pick a starting Date')
+   ending = gamma2.date_input('Pick an ending date')
+
+
+   rhs, rh, rhs1 = st.columns((2.5,3,2))
+   rh.markdown("""### Let's have a look at some raw data""")
+   #period = st.radio('Choose the Duration of Schemantics',('1d','1mo','1y'))
+
+   def load_data(ticker):
+       data = yf.download(ticker, starting, ending)
+       data.reset_index(inplace=True)
+       return data
+   data = load_data(tickerSymbol)
+   rds, rd, rds1 = st.columns((1,5,1))
+   rd.write(data)
+
+   tickerDF=tickerData.history(period='1d', start=starting, end=ending)
+
+
+   graphs1, graphs3 = st.columns(2)
+   graphs1.markdown("""### Opening Price """)
+   graphs1.line_chart(tickerDF.Open)
+   graphs3.markdown("""### Volume Price """)
+   graphs3.line_chart(tickerDF.Volume)
+   graphs1.markdown("""### Closing Price """)
+   graphs1.line_chart(tickerDF.Close)
+   graphs3.markdown("""### Highest Price """)
+   graphs3.line_chart(tickerDF.High)
+   graphs1.markdown("""### Lowest Price""")
+   graphs1.line_chart(tickerDF.Low)
+   graphs3.markdown("""### Adjusted Closing Price """)
+   graphs3.line_chart(data['Adj Close'])
+
+
+   START = "2015-01-01"
+   TODAY = date.today().strftime("%Y-%m-%d")
+
+   @st.cache
+   def load_data1(ticker):
+       data1 = yf.download(ticker, START, TODAY)
+       data1.reset_index(inplace=True)
+       return data1
+
+   data1 = load_data1(tickerSymbol)
+   #st.write(data1)
+
+   df_train = data1[['Date','Close']]
+   df_train = df_train.rename(columns = {'Date':'ds','Close':'y'})
+   #st.write(df_train)
+
+   predictor = Prophet()
+   predictor.fit(df_train)
+
+   st.markdown("""## Please Select the number of years for the predicted trends""")
+
+   n_years = st.slider('Years of prediction:', 1, 4)
+   period = n_years * 365
+   future = predictor.make_future_dataframe(periods = period)
+   forecast = predictor.predict(future)
+
+   fcs2 , afc, fcs3 = st.columns(3)
+   # Show and plot forecast
+   afc.title('Forecast data')
+   #st.write(forecast.tail())
+    
+   afc.markdown(f'Forecast plot for {n_years} following years')
+   fig1 = plot_plotly(predictor, forecast)
+   st.plotly_chart(fig1)
+
+
+   fcs,fch,fcs1 = st.columns(3)
+   fch.markdown("""## Forecated Components""")
+   fig2 = predictor.plot_components(forecast)
+   st.write(fig2)
+
+   st.caption('Made with Rivaldi')	
+	
