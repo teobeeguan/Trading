24e0b3e9bdddaa836f347cd1a32e895e04d4cd71
osamkz <93231011+osamkz@users.noreply.github.com>
2022-09-09 01:11:38 +0000

    Update StockScreenDemo.py

--- StockScreenDemo.py
+++ StockScreenDemo.py
@@ -49,7 +49,13 @@ names = ['SLM','AT']
 usernames = ['SLM','ATSLM']
 passwords = ['Hello123','Hola123']
 #hashed_passwords = stauth.hasher(passwords).generate()
-authenticator = stauth.Authenticate(names,usernames,passwords,'cookie_name', 'signature_key',cookie_expiry_days=30)
+authenticator = stauth.Authenticate(
+config[‘credentials’],
+config[‘cookie’][‘name’],
+config[‘cookie’][‘key’],
+config[‘cookie’][‘expiry_days’]),
+config[‘preauthorized’]
+)
 name, authentication_status = authenticator.login('Login','sidebar')
 if authentication_status:
  st.write("Welcome *%s*" % (name))
