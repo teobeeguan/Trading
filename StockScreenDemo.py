bb0f970251adeff5596edf4c2b22a9ff747b8347
osamkz <93231011+osamkz@users.noreply.github.com>
2022-09-09 00:18:42 +0000

    Update StockScreenDemo.py

--- StockScreenDemo.py
+++ StockScreenDemo.py
@@ -46,8 +46,10 @@ st.sidebar.write("""
 
 snp500 = pd.read_csv("Datasets/SP500.csv")
 cac40 = pd.read_csv("Datasets/CAC40.csv")
+ftse100 = pd.read_csv("Datasets/FTSE100.csv")
 symbols = snp500['Symbol'].sort_values().tolist()
 symbolsCAC40 = cac40['Symbol'].sort_values().tolist()
+symbolsFTSE100 = ftse100['Symbol'].sort_values().tolist()
 # for extracting data from finviz
 finviz_url = 'https://finviz.com/quote.ashx?t='
 
