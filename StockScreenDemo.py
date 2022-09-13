691a70d3f4cf4384ee2dcf9b7c6527828268aab3
osamkz <93231011+osamkz@users.noreply.github.com>
2022-09-13 02:38:15 +0000

    Update StockScreenDemo.py

--- StockScreenDemo.py
+++ StockScreenDemo.py
@@ -791,10 +791,7 @@ if check_password():
        st.header("Vue des Sentiments des tweets")
        st.subheader("Emotions qui ressortent le plus du stock {}".format(ticker))
        index_sent = dfAaplproc[1:10].values
-       fig3 = px.bar(x=index_sent,
-             y=dfAaplproc[1:10].sum(axis=1),
-             color_continuous_scale=px.colors.sequential.Pinkyl,
-             title='Sentiment volume')
+       fig3 = px.bar(dfAaplproc[1:10], x=index_sent, y=dfAaplproc[1:10].sum(axis=1), color_continuous_scale=px.colors.sequential.Pinkyl, title='Sentiment volume')
        fig2 = px.bar(dfAaplproc[1:10].sum(axis=1))
        st.plotly_chart(fig2)
        st.plotly_chart(fig3)
