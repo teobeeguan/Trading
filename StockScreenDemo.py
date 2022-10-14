a22a2a3fe6c46ec66bb9bb51e91218cadbf491fe
osamkz <93231011+osamkz@users.noreply.github.com>
2022-10-14 02:34:43 +0000

    Update StockScreenDemo.py

--- StockScreenDemo.py
+++ StockScreenDemo.py
@@ -589,7 +589,7 @@ if check_password():
 
        df_train = data1[['Date','Close']]
        df_train = df_train.rename(columns = {'Date':'ds','Close':'y'})
-       df_train=df_train['ds'].dt.tz_localize(None)
+       df_train['ds'].dt.tz_localize(None)
        st.write(df_train)
 
        predictor = Prophet()
